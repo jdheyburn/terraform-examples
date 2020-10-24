2cbc8b3d5bb32472d591adb29b26ad78898734e8
Joseph Heyburn <jdheyburn@gmail.com>
2020-10-24 17:26:25 +0100

    SSM automation post

--- aws-ssm-automation-1/ec2_iam.tf
+++ aws-ssm-automation-1/ec2_iam.tf
@@ -29,3 +29,37 @@ resource "aws_iam_instance_profile" "vm_base" {
   name = "vm-base"
   role = aws_iam_role.vm_base.name
 }
+
+data "aws_iam_policy_document" "ssm_scripts" {
+  statement {
+    sid    = "AllowS3"
+    effect = "Allow"
+
+    actions = ["s3:GetObject"]
+
+    resources = [
+      "${aws_s3_bucket.script_bucket.arn}/ssm_scripts/*",
+    ]
+  }
+
+  statement {
+    sid    = "AllowKMS"
+    effect = "Allow"
+
+    actions = ["kms:Decrypt"]
+
+    resources = [aws_kms_key.script_bucket_key.arn]
+  }
+}
+
+resource "aws_iam_policy" "ssm_scripts" {
+  name        = "PullSSMScripts"
+  description = "Enables instances to download SSM scripts from S3"
+
+  policy = data.aws_iam_policy_document.ssm_scripts.json
+}
+
+resource "aws_iam_role_policy_attachment" "instance_download_scripts" {
+  role       = aws_iam_role.vm_base.name
+  policy_arn = aws_iam_policy.ssm_scripts.arn
+}
