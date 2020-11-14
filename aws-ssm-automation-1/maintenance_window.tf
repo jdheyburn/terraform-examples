793c5608900062b3f99f3861d8a7fb1f1f9a9117
Joseph Heyburn <jdheyburn@gmail.com>
2020-11-14 15:55:13 +0000

    Add maintenance window

--- aws-ssm-automation-1/maintenance_window.tf
+++ aws-ssm-automation-1/maintenance_window.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_maintenance_window" "patch_with_healthcheck" {
   name              = "PatchWithHealthcheck"
   description       = "Daily patch event with a healthcheck afterward"
-  schedule          = "cron(0 9 ? * * *)"
+  schedule          = "cron(0 9 ? * * *)" # Everyday at 9am UK time
   schedule_timezone = "Europe/London"
   duration          = 3
   cutoff            = 1
@@ -22,22 +22,22 @@ resource "aws_ssm_maintenance_window_target" "patch_with_healthcheck_target" {
   }
 
   # Using tags is more scalable
-#   targets {
-#     key    = "tag:Terraform"
-#     values = ["true"]
-#   }
+  #   targets {
+  #     key    = "tag:Terraform"
+  #     values = ["true"]
+  #   }
 }
 
-resource "aws_ssm_maintenance_window_task" "patch_with_healthcheck_task" {
+resource "aws_ssm_maintenance_window_task" "patch_task" {
   window_id        = aws_ssm_maintenance_window.patch_with_healthcheck.id
   task_type        = "RUN_COMMAND"
-  task_arn         = aws_ssm_document.perform_healthcheck_s3.arn
+  task_arn         = "AWS-RunPatchBaseline"
   priority         = 10
-  service_role_arn = aws_iam_role.example.arn  
-  
-  max_concurrency  = "100%"
-  max_errors       = 0
-  
+  service_role_arn = aws_iam_role.patch_mw_role.arn
+
+  max_concurrency = "100%"
+  max_errors      = 0
+
   targets {
     key    = "WindowTargetIds"
     values = [aws_ssm_maintenance_window_target.patch_with_healthcheck_target.id]
@@ -45,21 +45,34 @@ resource "aws_ssm_maintenance_window_task" "patch_with_healthcheck_task" {
 
   task_invocation_parameters {
     run_command_parameters {
-      output_s3_bucket     = aws_s3_bucket.example.bucket
-      output_s3_key_prefix = "output"
-      service_role_arn     = aws_iam_role.example.arn
-      timeout_seconds      = 600
-
-      notification_config {
-        notification_arn    = aws_sns_topic.example.arn
-        notification_events = ["Failed", "TimedOut", "Cancelled"]
-        notification_type   = "Command"
-      }
+      timeout_seconds  = 3600
 
       parameter {
-        name   = "commands"
-        values = ["date"]
+        name = "Operation"
+        values = ["Scan"]
       }
+    }
+  }
+}
+
+resource "aws_ssm_maintenance_window_task" "healthcheck_task" {
+  window_id        = aws_ssm_maintenance_window.patch_with_healthcheck.id
+  task_type        = "RUN_COMMAND"
+  task_arn         = aws_ssm_document.perform_healthcheck_s3.arn
+  priority         = 20
+  service_role_arn = aws_iam_role.patch_mw_role.arn
+
+  max_concurrency = "100%"
+  max_errors      = 0
+
+  targets {
+    key    = "WindowTargetIds"
+    values = [aws_ssm_maintenance_window_target.patch_with_healthcheck_target.id]
+  }
+
+  task_invocation_parameters {
+    run_command_parameters {
+      timeout_seconds  = 600
     }
   }
 }
